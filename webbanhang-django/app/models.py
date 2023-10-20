5b574e80ae6fb8719975e42562149d89d7645211
DUCTOAN1010 <90306609+DUCTOAN1010@users.noreply.github.com>
2023-10-20 22:39:32 +0700

    Add files via upload

---
+++ webbanhang-django/app/models.py
@@ -0,0 +1,83 @@
+# tạo csdl
+from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm  
+
+#change forms register django
+class CreateUserForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username','email','first_name','last_name','password1','password2']
+
+# khach hang
+class Customer(models.Model):
+    user=models.OneToOneField(User,on_delete=models.SET_NULL,null=True,blank=False)
+    name = models.CharField(max_length=200,null=True)
+    emali = models.CharField(max_length=200,null=True)
+    
+    def __str__(self) :
+        return self.name
+#San pham    
+class Product(models.Model):
+    name = models.CharField(max_length=200,null=True)
+    price = models.FloatField()
+    digital = models.BooleanField(default=False,null=True,blank=False) #kiem tra co phai la do dien ko ?
+    image = models.ImageField(null=True,blank=True)
+    
+    def __str__(self) :
+        return self.name
+    @property
+    def ImageURL(self):
+        try:
+            url = self.image.url
+        except:
+            url = ''
+        return url         
+
+#dat hang
+class Order(models.Model):
+    customer=models.ForeignKey(Customer,on_delete=models.SET_NULL,blank=True,null=True)
+    date_order = models.DateTimeField(auto_now_add=True)
+    complete = models.BooleanField(default=False,null=True,blank=False) 
+    transaction_id = models.CharField(max_length=200,null=True)
+    
+    def __str__(self) :
+        return str(self.id)
+    # hàm tính tổng số lượng
+    @property
+    def get_cart_items(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.quantity for item in orderitems])
+        return total
+    # hàm tính tổng sản phẩm
+    @property
+    def get_cart_total(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.get_total for item in orderitems])
+        return total    
+    
+ #dat nhieu hang   
+class OrderItem(models.Model):
+    product=models.ForeignKey(Product,on_delete=models.SET_NULL,blank=True,null=True)
+    order=models.ForeignKey(Order,on_delete=models.SET_NULL,blank=True,null=True)
+    quantity = models.IntegerField(default=0,null=True,blank=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+    
+    # Định nghĩa hàm total - tổng sản phẩm 
+    @property
+    def get_total(self):
+        total = self.product.price * self.quantity
+        return total
+ 
+# Địa chỉ ship    
+class ShippingAddress(models.Model):
+    customer=models.ForeignKey(Customer,on_delete=models.SET_NULL,blank=True,null=True)
+    order=models.ForeignKey(Order,on_delete=models.SET_NULL,blank=True,null=True)
+    address = models.CharField(max_length=200,null=True)
+    city = models.CharField(max_length=200,null=True)
+    state = models.CharField(max_length=200,null=True)
+    phoneNumber = models.CharField(max_length=200,null=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self) :
+        return self.address
